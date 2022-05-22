08031c9523886b267e8c24c98b0dc81a75d979e0
Tim-McCann <tjm8679@gmail.com>
2022-05-22 12:20:49 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello there master kenobi
         </a>
       </header>
     </div>
